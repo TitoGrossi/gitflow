ab6c899e9fcdb5bcf82cc378085858f38855fd69
TitoGrossi <tito.brossi@gmail.com>
2022-05-01 13:46:16 -0300

    chore: dummy

--- custommath/sum.go
+++ custommath/sum.go
@@ -12,6 +12,22 @@ func Subtraction(a int, b int) int {
 	return a - b
 }
 
-func Division(a int, b int) int {
+func Division1(a int, b int) int {
+	return a / b
+}
+
+func Division2(a int, b int) int {
+	return a / b
+}
+
+func Division3(a int, b int) int {
+	return a / b
+}
+
+func Division4(a int, b int) int {
+	return a / b
+}
+
+func Division5(a int, b int) int {
 	return a / b
 }
